50a46507a569a17bc01c23d9c91b88e7f8a8a7ea
Todorovic-Nebojsha <necotodorovik@gmail.com>
2018-12-09 18:03:20 +0100

    Refresh is done with service.

--- app/src/main/java/mpip/finki/ukim/mk/lab2_1/MainActivity.java
+++ app/src/main/java/mpip/finki/ukim/mk/lab2_1/MainActivity.java
@@ -2,10 +2,14 @@ package mpip.finki.ukim.mk.lab2_1;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -32,6 +36,7 @@ import mpip.finki.ukim.mk.lab2_1.Model.Movie;
 import mpip.finki.ukim.mk.lab2_1.Model.MovieList;
 
 import mpip.finki.ukim.mk.lab2_1.Repository.MovieApiInterface;
+import mpip.finki.ukim.mk.lab2_1.Service.DownloadMovieService;
 import mpip.finki.ukim.mk.lab2_1.persistence.MovieItemRepository;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,7 +44,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static mpip.finki.ukim.mk.lab2_1.Service.DownloadMovieService.DATABASE_UPDATED;
+
 public class MainActivity extends AppCompatActivity {
+    private UpdatedItemsBroadcastReceiver receiver;
+
     Retrofit retrofit;
     MovieItemRepository movieItemRepository;
 
@@ -67,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        receiver=new UpdatedItemsBroadcastReceiver();
+
+
         logger=Logger.getLogger(MainActivity.class.getName());
         movies=new ArrayList<>();
 
@@ -90,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        IntentFilter filter = new IntentFilter(DATABASE_UPDATED);
+        this.registerReceiver(receiver,filter);
 
     }
 
@@ -139,16 +153,18 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent intent = new Intent(CardViewActivity.this,DownloadFlickrItemsService.class);
-//                intent.putExtra(getString(R.string.download_flickr_tag),"ФИНКИ");
-//                CardViewActivity.this.startService(
-//                        intent);
-//                Snackbar.make(view, "Syncing", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                // search proba ovoj treba da bide refresh!!!
-                if(searchString!=null && searchString.length()>0){
-                    search(searchString);
+                if(searchString!=null && searchString.length()>0) {
+                    Intent intent = new Intent(MainActivity.this, DownloadMovieService.class);
+                    intent.putExtra("search", searchString);
+                    MainActivity.this.startService(
+                            intent);
+                    Snackbar.make(view, "Syncing", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
                 }
+                // search proba ovoj treba da bide refresh!!!
+//                if(searchString!=null && searchString.length()>0){
+//                    search(searchString);
+//                }
                 else{
                     Toast.makeText(MainActivity.this,"You have not made a search,you cannot sync!",Toast.LENGTH_LONG).show();
                 }
@@ -258,7 +274,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this,"page: "+page_num,Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    Toast.makeText(MainActivity.this,"There are no more images...",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this,"There are no more movies...",Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -270,4 +286,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public class UpdatedItemsBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            movieItemRepository.listAllMovieItems().observe(MainActivity.this, new Observer<List<Movie>>() {
+                @Override
+                public void onChanged(@Nullable List<Movie> flickrItems) {
+                    cardViewAdapter.updateData(flickrItems);
+                    Toast.makeText(MainActivity.this,"Synced",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
 }
---
+++ app/src/main/java/mpip/finki/ukim/mk/lab2_1/Service/DownloadMovieService.java
@@ -0,0 +1,86 @@
+package mpip.finki.ukim.mk.lab2_1.Service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import mpip.finki.ukim.mk.lab2_1.MainActivity;
+import mpip.finki.ukim.mk.lab2_1.Model.Movie;
+import mpip.finki.ukim.mk.lab2_1.Model.MovieList;
+import mpip.finki.ukim.mk.lab2_1.R;
+import mpip.finki.ukim.mk.lab2_1.Repository.MovieApiInterface;
+import mpip.finki.ukim.mk.lab2_1.persistence.MovieItemRepository;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DownloadMovieService extends IntentService {
+    public static final String DATABASE_UPDATED="mpip.finki.ukim.mk.lab2_1.DATABASE_UPDATED";
+
+    private MovieApiInterface api;
+
+    private MovieItemRepository movieItemRepository;
+
+    public DownloadMovieService(){
+        super("Download and save movies");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(MovieApiInterface.Base_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        api = retrofit.create(MovieApiInterface.class);
+
+        movieItemRepository = new MovieItemRepository(DownloadMovieService.this);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+//        NotificationUtils.makeNotification(this,
+//                "my_channel_05",
+//                "Downloading",
+//                "Downloading Countries from API",
+//                R.drawable.ic_search_black_24dp,
+//                new Intent(this, MainActivity.class),
+//                1234,
+//                b -> b.setAutoCancel(true));
+      Bundle b=intent.getExtras();
+      //final String scroll=b.getString("Scroll");
+      int page_num=1;
+      String search=b.getString("search");
+        //List<Movie> movies =new ArrayList<>();
+        api.getMovies(search,page_num).enqueue(new Callback<MovieList>() {
+            @Override
+            public void onResponse(Call<MovieList> call, Response<MovieList> response) {
+                if(response.isSuccessful()){
+                    movieItemRepository.deleteAll();
+
+                    for(Movie m : response.body().getMovies()){
+                        movieItemRepository.insertItem(m);
+                    }
+                    sendDatabaseUpdatedBroadcast();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<MovieList> call, Throwable t) {
+                //Toast.makeText(getApplicationContext(),t.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void sendDatabaseUpdatedBroadcast() {
+        sendBroadcast(new Intent(DATABASE_UPDATED));
+    }
+
+
+    }
